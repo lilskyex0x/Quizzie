6c095adde237fa415be45fcd3cae1e10aac7272d
lilskyex0x <lilskyex0x.apk@gmail.com>
2024-01-24 19:32:28 +0630

    feat: create radio answers for quiz answers

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ const userScore = document.querySelector(".score");
 const questionText = document.querySelector(".question");
 
 const answerChoices = document.querySelector("#choices");
+
+for (let i = 1; i < 5; i++) {
+  const radioBox = document.createElement("input");
+  const li = document.createElement("li");
+
+  radioBox.type = "radio";
+  radioBox.name = "answer";
+  radioBox.value = "answer" + i;
+  radioBox.classList.add("radio__box");
+  li.innerHTML = `Answer${i} `;
+  li.appendChild(radioBox);
+  answerChoices.appendChild(li);
+}
