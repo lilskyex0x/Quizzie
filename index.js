7009eb71feb4f8d76c9574d6304ea37b1e67c0ab
lilskyex0x <lilskyex0x.apk@gmail.com>
2024-01-25 14:13:08 +0630

    feat: create question container paired with answers

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ const nextBtn = document.querySelector(".next");
 const previousBtn = document.querySelector(".previous");
 const resetBtn = document.querySelector(".reset__btn");
 const userScore = document.querySelector(".score");
-const questionText = document.querySelector(".question");
 
 const answerChoices = document.querySelector("#choices");
 
-for (let i = 1; i < 5; i++) {
-  const radioBox = document.createElement("input");
-  const li = document.createElement("li");
+window.onload = async () => {
+  const response = await fetch(api);
+  const data = await response.json();
 
-  radioBox.type = "radio";
-  radioBox.name = "answer";
-  radioBox.value = "answer" + i;
-  radioBox.classList.add("radio__box");
-  li.innerHTML = `Answer${i} `;
-  li.appendChild(radioBox);
-  answerChoices.appendChild(li);
-}
+  init(data);
+};
+
+const init = (data) => {
+  const questionText = document.querySelector(".question");
+  let currentQuestionIndex = 0;
+  let questions = {};
+  let questionsDiv = "";
+
+  questions = data.results;
+  questionsDiv = questions.map((question) => {
+    return `
+    <div class="question__container">
+      <h3 class="question">${question.question}</h3>
+      <ul class="answer__choices">
+        <li class="answer__choice"><input type="radio" value="${question.correct_answer}"/>${question.correct_answer}</li>
+        <li class="answer__choice"><input type="radio" value="${question.incorrect_answers[0]}"/>${question.incorrect_answers[0]}</li>
+        <li class="answer__choice"><input type="radio" value="${question.incorrect_answers[1]}"/>${question.incorrect_answers[1]}</li>
+        <li class="answer__choice"><input type="radio" value="${question.incorrect_answers[2]}"/>${question.incorrect_answers[2]}</li>
+      </ul>
+    </div>
+    `;
+  });
+  return (questionText.innerHTML = questionsDiv[currentQuestionIndex]);
+};
